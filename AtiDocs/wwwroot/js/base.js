8ffbb9c1a19c81cf3ec615507f19031f9d63f674
ati <molnar.attila28@gmail.com>
2021-09-13 20:57:19 +0200

    Fix https prefix

--- AtiDocs/wwwroot/js/base.js
+++ AtiDocs/wwwroot/js/base.js
@@ -1,6 +1,6 @@
 function CopyLink(anchor) {
     var tempText = document.createElement("input");
-    tempText.value = window.location.protocol + window.location.hostname + ':' + window.location.port + window.location.pathname + '#' + anchor;
+    tempText.value = window.location.protocol + "//" + window.location.hostname + ':' + window.location.port + window.location.pathname + '#' + anchor;
     document.body.appendChild(tempText);
     tempText.select();
     document.execCommand("copy");
